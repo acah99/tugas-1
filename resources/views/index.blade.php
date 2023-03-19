8940f582f52f31503b0a74ccc185c41912af282d
acah99 <lalumarsyahiwanramadhan@gmail.com>
2023-03-18 23:09:40 -0700

    tugas pertemuan 3

---
+++ resources/views/index.blade.php
@@ -0,0 +1,398 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Lalu Marsyah Iwan Ramadhan</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/As.jpg" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+{{-- ini adalah tag komentar, tag komentar tidak akan dijalankan di web serve --}}
+<!-- tag komentar lainnya -->
+
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="">
+    <div class="d-flex flex-column"> 
+
+      <div class="profile">
+        <img src="assets/img/bg1.jpg" alt="" width="200" height="100" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">LALU MARSYAH IWAN RAMADHAN</a></h1>
+       
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Beranda</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Tentang Saya</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Biografi</span></a>
+          </li>
+          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i>
+              <span>Portfolio</span></a></li>   
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Kontak</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-edit"></i> <span>Pengaturan</span></a>
+          </li>
+        </ul>
+      </nav>
+      <!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>LALU MARSYAH IWAN RRAMADHAN</h1>
+      <p>I'm <span class="typed" data-typed-items="Student, Developer, Freelancer"></span></p>
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>About</h2>
+          <p>Nikmati setiap proses dalam berkreativitas jangan hanya berfokus dari ide yang pertama kita dapatkan. kita
+            akan mendapatkan hal menarik lainnya saat kita berhasil menikmati proses tersebut.</p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/" class="img-fluid shadow-lg" alt="" width="400" height="400">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3>Web Developer.</h3>
+            <p class="fst-italic">
+              Saya seoramg design grafis dan app/web developmend, tidak terikat kontrak kerja dengan siapapun namun dapat berkerja dengan
+              siapapun.</p>
+            <div class="row">
+              <div class="col-lg">
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Nama:</strong> <span>Lalu Marsyah Iwan Ramadhan</span>
+                  </li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Tempat/Tgl Lahir:</strong> <span>Kerongkong, 03 Oktober
+                      1999</span>
+                  </li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>lalumarsyahiwanramadhan@gmail.com</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>No Hp:</strong> <span>+62 818 8341 6808</span></li>
+                </ul>
+                <ul>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>23 Tahun</span></li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Status Pernikahan:</strong> <span>Belum Kawin</span>
+                  </li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Pekerjaan:</strong> <span>Freelance</span>
+                  </li>
+                  <li><i class="bi bi-chevron-right"></i> <strong>Alamat:</strong> <span>Kerongkng, Kec
+                      Suralaga, Kab lombok timur, Nusa Tenggara Barat</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <hr><br>
+          <p>Di tengah kesibukan sehari-hari, rasanya tidak mudah sekali mengorbankan waktu demi keperluan lain. Mungkin
+            kita berpikir bekerja atau aktivitas lain lebih penting dan mendesak. Tak ayal waktu untuk
+            bersenang-senang dan menghilangkan stress pun tersita.
+            <br><br>
+            Jangan terus memikirkan hal lain, tapi kita juga harus mencintai dan menghargai diri sendiri. Pekerjaan
+            itu penting, tapi menghargai tubuh jauh lebih penting. Jika kita terlalu lelah dan jatuh sakit, semua
+            pekerjaan secara otomatis justru bisa terganggu.
+          </p>
+        </div>
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Facts Section ======= -->
+    <section id="facts" class="facts">
+
+
+      <!-- ======= Resume Section ======= -->
+      <section id="resume" class="resume">
+        <div class="container">
+
+          <div class="section-title">
+            <h2>Daftar Riwayat Hidup</h2>
+            <p>Menjadi salah satu diantara ahli bidang <b>Design dan Programing</b> dengan pengembangan di bidang Website dan
+              Mobile.
+              Fokus utama yang berkaitan dengan<i> Web Developer, Web Desainer dan Database</i>. Oleh karenanya
+              minat yang saya ambil adalah bidang IT. Untuk menempuh itu tentu ditunjang skill teknikal, terutama dalam
+              hal Website,desain,dan mobile app database programming dan semua yang berkaitan dengan Programing.</p>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6" data-aos="fade-up">
+              <h3 class="resume-title">Summary</h3>
+              <div class="resume-item pb-0">
+                <h4>Lalu Marsyah Iwan Ramadhan</h4>
+                <p><em>Terima kasih kepada para dosen dan tenaga pengajar yang telah mengajari kami
+                    Mengenai koding dan bahasa pemograman. Walaupun terkadang kalian killer,
+                    Tapi kemampuan program ini akan kami bawa ke dunia kerja.</em></p>
+                <p><em>Menyelesaikan setiap permasalahan dengan coding adalah suatu kesenangan tersendiri bagi seorang
+                    programmer. Dan harus belajar untuk bersabar dalam menemukan setiap permasalahan atau Error yang
+                    kita hadapi</em></p>
+                  </div>
+
+              <h3 class="resume-title">Education</h3>
+              <table class="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Pendidikan</th>
+                    <th scope="col">Tahun</th
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row resume-item">1</th>
+                    <td>SD Negeri 1 Kerongkong</td>
+                    <td>2006 - 2011</td>
+                    
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>SMP Negeri 2 Selong</td>
+                    <td>2011 - 2014</td>
+                    <td><p><em>Jl. Pangeran Diponegoro RT 04, Tabur Lestari, Kec. Sei Menggaris, Kab. Nunukan Prov.</em></p></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>MAN 1 LOMBOK TIMUR</td>
+                    <td>2014 - 2017</td>
+                    
+                  </tr>
+            
+                </tbody>
+              </table>  
+            
+            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+              <h3 class="resume-title">Work experience</h3>
+              <div class="resume-item">
+                <h4>PT. VANDO</h4>
+                <h5>2022 - 2023</h5>
+                
+                <p><b>PT.VANDO</b> salah satu perusahaan yang bergerak di bidang peroperti.
+                </p>
+                <ul>
+                  <li><b>APP mobile development,</b> Disini saya membuat aplikasi android untuk perusahaan sebagai media promosi properti perusahaan.</li>
+                  <li><b>Web dwvwlopmwnd,</b> Disini saya juga membuat website yang di butuhkan oleh perusahaan sebagai penunjang kebutuhan dari perusahaan.</li>
+                 
+                </ul>
+              </div>
+             
+              
+            </div>
+          </div>
+      </section><!-- End Resume Section -->
+
+      <!-- ======= Portfolio Section ======= -->
+      <section id="portfolio" class="portfolio section-bg">
+        <div class="container">
+
+          <div class="section-title">
+            <h2>Portfolio</h2>
+            <p>Berikut foto dan hasil karya yang telah disiapkan untuk di gunakan sesuai dengan keperluan yang anda
+              inginkan.</p>
+          </div>
+
+          <div class="row" data-aos="fade-up">
+            <div class="col-lg-12 d-flex justify-content-center">
+              <ul id="portfolio-flters">
+                <li data-filter="*" class="filter-active">All</li>
+                <li data-filter=".filter-app">App</li>
+                <li data-filter=".filter-card">Card</li>
+                <li data-filter=".filter-web">Web</li>
+              </ul>
+            </div>
+          </div>
+
+          
+      <!-- Facts -->
+      <div class="container section-bg">
+
+        <div class="section-title">
+          <h2>Facts</h2>
+          <p>Nikmati setiap proses dalam berkreativitas jangan hanya berfokus dari ide yang pertama kita dapatkan. kita
+            akan mendapatkan hal menarik lainnya saat kita berhasil menikmati proses tersebut. <b>Sukses buat kita
+              semua.</b></p>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
+            <div class="count-box">
+              <i class="bi bi-emoji-smile"></i>
+              <span data-purecounter-start="0" data-purecounter-end="2232" data-purecounter-duration="1.5"
+                class="purecounter"></span>
+              <p><strong>Happy Clients</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="100">
+            <div class="count-box">
+              <i class="bi bi-journal-richtext"></i>
+              <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1.5"
+                class="purecounter"></span>
+              <p><strong>Projects</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="200">
+            <div class="count-box">
+              <i class="bi bi-chat"></i>
+              <span data-purecounter-start="0" data-purecounter-end="153" data-purecounter-duration="1.5"
+                class="purecounter"></span>
+              <p><strong>comment</strong></p>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up" data-aos-delay="300">
+            <div class="count-box">
+              <i class="bi bi-people"></i>
+              <span data-purecounter-start="0" data-purecounter-end="12" data-purecounter-duration="1.5"
+                class="purecounter"></span>
+              <p><strong>helpful work team</strong></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Facts -->
+
+      <!-- ======= Contact Section ======= -->
+      <section id="contact" class="contact">
+        <div class="container">
+
+          <div class="section-title">
+            <h2>Contact</h2>
+            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+              consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat
+              sit in iste officiis commodi quidem hic quas.</p>
+          </div>
+
+          <div class="row" data-aos="fade-in">
+
+            <div class="col-lg-5 d-flex align-items-stretch">
+              <div class="info">
+                <div class="address">
+                  <i class="bi bi-geo-alt"></i>
+                  <h4>Location:</h4>
+                  <p>Mataram, Nusa Tenggara Barat</p>
+                </div>
+
+                <div class="email">
+                  <i class="bi bi-envelope"></i>
+                  <h4>Email:</h4>
+                  <p>lalumarsyahiwanramadhan@gmail.com</p>
+                </div>
+
+                <div class="phone">
+                  <i class="bi bi-phone"></i>
+                  <h4>Call:</h4>
+                  <p>+62 818 8341 6808</p>
+                </div>
+
+                <!-- <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe> -->
+              </div>
+
+            </div>
+
+            <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+              <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="name">Your Name</label>
+                    <input type="text" name="name" class="form-control" id="name" required>
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="name">Your Email</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="name">Subject</label>
+                  <input type="text" class="form-control" name="subject" id="subject" required>
+                </div>
+                <div class="form-group">
+                  <label for="name">Message</label>
+                  <textarea class="form-control" name="message" rows="10" required></textarea>
+                </div>
+                <div class="my-3">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+                </div>
+                <div class="text-center"><button type="submit">Send Message</button></div>
+              </form>
+            </div>
+
+          </div>
+
+        </div>
+      </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Portfolio</span></strong>
+      </div>
+      <div class="credits">
+        Designed by <a href="#">Lalu Marsyah Iwan Ramadhan</a>
+      </div>
+    </div>
+  </footer>
+  <!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
